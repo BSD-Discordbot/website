0d9c0f4f066c84e4672cfa4bd8e1e59c201d1246
Legonzaur <legonzaur@live.fr>
2023-09-16 20:48:02 +0200

    add strict boolean expressions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    project: './tsconfig.eslint.json',
+    parser: "@typescript-eslint/parser",
     ecmaVersion: 'latest'
+  },
+  rules:{
+    "@typescript-eslint/strict-boolean-expressions": "error",
   }
 }
